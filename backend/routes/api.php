27394b0176dae1aaeac7b3ed291ffa0593c18b6c
Vestephan RAHARISON <raharisonfredericvestephan@gmail.com>
2026-03-05 19:22:48 -0500

    auth:sanctum

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,18 +4,53 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfilController;
 use App\Http\Controllers\ActiviteController;
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
+// ROUTE TEST
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'API ETN fonctionne'
+    ]);
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| ROUTES PUBLIQUES
+|--------------------------------------------------------------------------
+*/
+
 // PROFILS
 Route::get('/profils', [ProfilController::class, 'index']);
-Route::post('/profils', [ProfilController::class, 'store']);
 Route::get('/profils/{id}', [ProfilController::class, 'show']);
-Route::put('/profils/{id}', [ProfilController::class, 'update']);
-Route::delete('/profils/{id}', [ProfilController::class, 'destroy']);
 Route::get('/profils/search/{keyword}', [ProfilController::class, 'search']);
 
 // ACTIVITES
 Route::get('/activites', [ActiviteController::class, 'index']);
-Route::post('/activites', [ActiviteController::class, 'store']);
 Route::get('/activites/{id}', [ActiviteController::class, 'show']);
-Route::put('/activites/{id}', [ActiviteController::class, 'update']);
-Route::delete('/activites/{id}', [ActiviteController::class, 'destroy']);
 Route::get('/activites/search/{keyword}', [ActiviteController::class, 'search']);
+
+
+/*
+|--------------------------------------------------------------------------
+| ROUTES PROTEGEES (AUTHENTIFICATION)
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    // PROFILS
+    Route::post('/profils', [ProfilController::class, 'store']);
+    Route::put('/profils/{id}', [ProfilController::class, 'update']);
+    Route::delete('/profils/{id}', [ProfilController::class, 'destroy']);
+
+    // ACTIVITES
+    Route::post('/activites', [ActiviteController::class, 'store']);
+    Route::put('/activites/{id}', [ActiviteController::class, 'update']);
+    Route::delete('/activites/{id}', [ActiviteController::class, 'destroy']);
+
+});
